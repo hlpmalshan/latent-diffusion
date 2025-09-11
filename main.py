02b704f652b0d58da75137b344258ececc76a9bb
hlpmalshan <hlpmalshan@gmail.com>
2025-09-11 15:50:52 +0530

    added modified python files

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_parser(**parser_kwargs):
     def str2bool(v):
         if isinstance(v, bool):
             return v
-        if v.lower() in ("yes", "true", "t", "y", "1"):
+        if v.lower() in ("yes", "true", "d", "y", "1"):
             return True
         elif v.lower() in ("no", "false", "f", "n", "0"):
             return False
