4bb0673795a81054811e985394e6fce76bcf7da6
ShakthiPerera <shakthiperera231@gmail.com>
2025-11-10 05:01:02 +0000

    Update .txt creation of celeba

--- create_txt_file.py
+++ create_txt_file.py
@@ -1,12 +1,20 @@
 import os
 
 # Folders containing the images
-train_dir = "data/cifar-100-python/train_images"
-test_dir = "data/cifar-100-python/test_images"
+# train_dir = "data/cifar-100-python/train_images"
+# test_dir = "data/cifar-100-python/test_images"
+
+train_dir = "data/celeba/train"
+test_dir = "data/celeba/test"
+val_dir = "data/celeba/val"
 
 # Output txt files
-train_txt = "data/cifar-100-python/train_paths.txt"
-test_txt = "data/cifar-100-python/test_paths.txt"
+# train_txt = "data/cifar-100-python/train_paths.txt"
+# test_txt = "data/cifar-100-python/test_paths.txt"
+
+train_txt = "data/celeba/train.txt"
+test_txt = "data/celeba/test.txt"
+val_txt = "data/celeba/val.txt"
 
 # Function to save relative paths to a txt file
 def save_paths(folder, output_file):
@@ -20,6 +28,6 @@ def save_paths(folder, output_file):
 # Save paths
 save_paths(train_dir, train_txt)
 save_paths(test_dir, test_txt)
+save_paths(val_dir, val_txt)
 
-print(f"✅ Train image paths saved to {train_txt}")
-print(f"✅ Test image paths saved to {test_txt}")
+print("Completed")
